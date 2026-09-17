c1af13f34dc174c8ef3cfd0b3bfe5e447b4fd4d3
Ears2006 <ejclinton1@gmail.com>
2026-09-17 16:34:44 -0700

    Updated index.ts

--- supabase/functions/fantasy-ai/index.ts
+++ supabase/functions/fantasy-ai/index.ts
@@ -174,6 +174,16 @@ async function getFantasyProsProjection(
   week: number,
   scoringFormat: string
 ) {
+  const fantasyProsPlayerId = await findFantasyProsPlayerId(
+  playerName,
+  position
+);
+
+console.log(
+  "FANTASYPROS PLAYER ID:",
+  playerName,
+  fantasyProsPlayerId
+);
   const fpScoring =
   scoringFormat === "Half-PPR"
     ? "HALF"
